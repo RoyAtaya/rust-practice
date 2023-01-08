3f8a0b6df1ac8fffdb346c1272e72132fe1b2d1e
RoyAtaya <roy.ataya@gmail.com>
2023-01-07 17:04:57 -0800

    chapter 1 done

---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\royat\Documents\Developer\rust-practice\hello_cargo\target\release\hello_cargo.exe: C:\Users\royat\Documents\Developer\rust-practice\hello_cargo\src\main.rs
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\royat\Documents\Developer\rust-practice\hello_world\target\debug\hello_world.exe: C:\Users\royat\Documents\Developer\rust-practice\hello_world\src\main.rs
