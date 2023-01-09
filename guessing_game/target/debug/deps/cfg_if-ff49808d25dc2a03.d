2c07a48f41a12788cd111573f1e14ce33605dd4d
RoyAtaya <roy.ataya@gmail.com>
2023-01-09 13:48:49 -0800

    chapter 2 completed

---
+++ guessing_game/target/debug/deps/cfg_if-ff49808d25dc2a03.d
@@ -0,0 +1,7 @@
+C:\Users\royat\Documents\Developer\rust-practice\guessing_game\target\debug\deps\cfg_if-ff49808d25dc2a03.rmeta: C:\Users\royat\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\royat\Documents\Developer\rust-practice\guessing_game\target\debug\deps\libcfg_if-ff49808d25dc2a03.rlib: C:\Users\royat\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\royat\Documents\Developer\rust-practice\guessing_game\target\debug\deps\cfg_if-ff49808d25dc2a03.d: C:\Users\royat\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\royat\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs:
